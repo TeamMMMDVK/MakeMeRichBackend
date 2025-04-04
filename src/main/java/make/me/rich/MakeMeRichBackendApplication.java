c294d9c3745f758a233fe4886d68ae42fad26483
ViAarisKea <viaa0001@stud.kea.dk>
2025-04-04 14:29:24 +0200

    first commit

---
+++ src/main/java/make/me/rich/MakeMeRichBackendApplication.java
@@ -0,0 +1,13 @@
+package make.me.rich;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MakeMeRichBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MakeMeRichBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/make/me/rich/MakeMeRichBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package make.me.rich;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MakeMeRichBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
