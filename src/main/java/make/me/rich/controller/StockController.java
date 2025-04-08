74e4a7574ee1811baacb6210f4c04155e89f8938
MetteMunch <meha0005@stud.kea.dk>
2025-04-08 08:34:46 +0200

    anvendelse af URI og URL er gjort mere dynamisk

--- src/main/java/make/me/rich/controller/StockController.java
+++ src/main/java/make/me/rich/controller/StockController.java
@@ -1,7 +1,21 @@
 package make.me.rich.controller;
 
+import make.me.rich.service.YahooFinanceAPIService;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class StockController {
+
+
+    private YahooFinanceAPIService yahooFinanceAPIService;
+
+    public StockController(YahooFinanceAPIService yahooFinanceAPIService) {
+        this.yahooFinanceAPIService = yahooFinanceAPIService;
+    }
+
+    @GetMapping("/test")
+    public String testGetStockInfo() {
+        return yahooFinanceAPIService.fetchSymbolProfileFromYahooAPI();
+    }
 }
--- src/main/java/make/me/rich/service/YahooFinanceAPIService.java
+++ src/main/java/make/me/rich/service/YahooFinanceAPIService.java
@@ -12,18 +12,18 @@ public class YahooFinanceAPIService {
 
     private final WebClient webClient;
     private static final Logger logger = LoggerFactory.getLogger("Yahoo Service");
-    private static final String URL = "https://yahoo-finance-api-data.p.rapidapi.com/summary/symbol-profile";
+    private static final String URL = "https://yahoo-finance-api-data.p.rapidapi.com/";
     private static final String API_KEY = System.getenv("YAHOO_KEY");
     private static final String HOST = "yahoo-finance-api-data.p.rapidapi.com";
 
     public YahooFinanceAPIService(WebClient.Builder webClientBuilder) {
-        this.webClient = webClientBuilder.build();
+        this.webClient = webClientBuilder.baseUrl(URL).build();
     }
 
     public String fetchSymbolProfileFromYahooAPI() {
         Mono<String> response = webClient
                 .get()
-                .uri( URL + "?symbol=GOOG")//TODO: make dynamic
+                .uri( "summary/symbol-profile?symbol=GOOG")
                 .header("x-rapidapi-key", API_KEY)
                 .accept(MediaType.APPLICATION_JSON)
                 .header("Accept-Encoding", "")
