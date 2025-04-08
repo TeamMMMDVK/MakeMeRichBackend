9a65d03f576336360767beb08c20ad4557f55f2a
MetteMunch <meha0005@stud.kea.dk>
2025-04-08 09:53:36 +0200

    Har indarbejdet rolle i openai client

--- src/main/java/make/me/rich/service/OpenAIService.java
+++ src/main/java/make/me/rich/service/OpenAIService.java
@@ -7,24 +7,29 @@ import org.springframework.stereotype.Service;
 
 @Service //make the prompt and send it to chatgpt
 public class OpenAIService {
-    private final ChatClient chatClient;
+    private final ChatClient chatClient; //Springs OpenAI-klient
     private final YahooFinanceAPIService yahooFinanceAPIService;
     private final TwelveDataAPIService twelveDataAPIService;
     private static final Logger logger = LoggerFactory.getLogger("OpenAI Service");
 
 
     public OpenAIService(ChatClient.Builder chatClientBuilder, YahooFinanceAPIService yahooFinanceAPIService, TwelveDataAPIService twelveDataAPIService) {
-        this.chatClient = chatClientBuilder.build();
+        this.chatClient = chatClientBuilder.build(); //chatClient bygges med chatClientBuilder, så har vi mulighed for at konfigurere inden brug
         this.yahooFinanceAPIService = yahooFinanceAPIService;
         this.twelveDataAPIService = twelveDataAPIService;
     }
 
-    public String getPrompt(String userPrompt) {
-        String prompt = this.chatClient.prompt()
-                .user(userPrompt + yahooFinanceAPIService.fetchSymbolProfileFromYahooAPI("META") + twelveDataAPIService.fetchSymbolHistoricDataFromTwelveDataAPI("META"))
-                .call()
-                .content();
-        logger.info(prompt);
-        return prompt;
+    public String getPrompt(String userPrompt, String symbol) {
+        String fullPrompt = userPrompt
+                + yahooFinanceAPIService.fetchSymbolProfileFromYahooAPI(symbol)
+                + twelveDataAPIService.fetchSymbolHistoricDataFromTwelveDataAPI(symbol);
+
+        String response = this.chatClient.prompt() //her sammensættes vores prompt
+                .system("You are an experienced stockanalytic who answers and explain short and precise")
+                .user(fullPrompt)
+                .call() //her laves kaldet
+                .content(); //her modtages svaret
+        logger.info(response);
+        return response;
     }
 }
