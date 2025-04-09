f20bf263afcb9d7dc5c34fc75988502322d23984
DanielJensenKEA <daje0003@stud.kea.dk>
2025-04-09 14:44:44 +0200

    * Tilføjelse af LOMBOK * Model klasser tilføjet til TwelveDataAPIResponse.
    Praktisk test. Bliver sendt korrekt til frontend.

--- src/main/java/make/me/rich/controller/StockController.java
+++ src/main/java/make/me/rich/controller/StockController.java
@@ -3,6 +3,7 @@ package make.me.rich.controller;
 import make.me.rich.model.StockPriceValue;
 import make.me.rich.service.TwelveDataAPIService;
 import make.me.rich.service.YahooFinanceAPIService;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
+@CrossOrigin
 public class StockController {
 
 
