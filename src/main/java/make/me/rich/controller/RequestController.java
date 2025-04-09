ee7fe724276762bd08f98eea0df65c9ac860d263
MetteMunch <meha0005@stud.kea.dk>
2025-04-09 06:17:10 +0200

    RequestDTO

--- src/main/java/make/me/rich/controller/RequestController.java
+++ src/main/java/make/me/rich/controller/RequestController.java
@@ -1,12 +1,14 @@
 package make.me.rich.controller;
 
+import make.me.rich.dto.ChatRequestDTO;
 import make.me.rich.service.RequestService;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("app/v1/request")
+@RequestMapping("app/v1/")
 public class RequestController {
 
     private final RequestService requestService;
@@ -15,6 +17,8 @@ public class RequestController {
         this.requestService = requestService;
     }
 
-//    @PostMapping("/create")
-//    public
+    @PostMapping("/request")
+    public ChatRequestDTO makeRequest(String prompt, String symbol) {
+        return requestService.createChatRequest(prompt, symbol);
+    }
 }
